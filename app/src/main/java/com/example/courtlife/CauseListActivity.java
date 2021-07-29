541e2819d074f95a52b0527a08edbb52f151bdf1
khalid063 <khalid.ahmad063@gmail.com>
2021-07-29 17:23:09 +0600

    Login design done

---
+++ app/src/main/java/com/example/courtlife/CauseListActivity.java
@@ -0,0 +1,14 @@
+package com.example.courtlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CauseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cause_list);
+    }
+}
