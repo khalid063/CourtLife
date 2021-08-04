e78166678050139e7ea3c46be50c96631ce5f6fb
khalid063 <khalid.ahmad063@gmail.com>
2021-08-04 19:23:42 +0600

    Registration activity design 20% work is done

--- app/src/main/java/com/example/courtlife/RegistrationActivity.java
+++ app/src/main/java/com/example/courtlife/RegistrationActivity.java
@@ -2,13 +2,51 @@ package com.example.courtlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button butRegAsAdvocate, butRegAsUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+        //------------------------------------ Find view by id --------------------------------//
+        butRegAsAdvocate = findViewById(R.id.butRegAsAdvocate);
+        butRegAsUser = findViewById(R.id.butRegAsUser);
+
+
+
+        //------------------------------------- Set on click listener -------------------------//
+        butRegAsAdvocate.setOnClickListener(this);
+        butRegAsUser.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int a = v.getId();
+
+        switch (a){
+            case R.id.butRegAsAdvocate :
+                Intent iadvocate = new Intent(RegistrationActivity.this, RegistrationAsAdvocate.class);
+                startActivity(iadvocate);
+                break;
+            case R.id.butRegAsUser :
+                Intent iuser = new Intent(RegistrationActivity.this, RegistrationAsAdvocate.class);
+                startActivity(iuser);
+                break;
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/courtlife/RegistrationAsUser.java
@@ -0,0 +1,14 @@
+package com.example.courtlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationAsUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_as_user);
+    }
+}
